00b627f41da7c3736db6e5e26d8730603009016e
Dessian-Guiro <135682316+Dessian-Guiro@users.noreply.github.com>
2024-01-29 02:39:10 -0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
+
+
+
 // Write your Color component here
+
+
+const Color = ({color}) => {
+  return <div className={color}></div>
+}
+
 
 const App = () => {
   return (
+    
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+      <Color color="red"/>
+  <Color color="blue"/>
+  <Color color="green"/>
+      </div>
     </div>
   );
 };
